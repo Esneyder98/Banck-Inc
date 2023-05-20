e13dd86ebd3bcfe53faf9332fa5688009ac8bcfc
Esneyder98 <esneydersa@hotmail.com>
2023-05-20 13:51:13 -0500

    Creando proyecto base para iniciaar creacion de API

---
+++ src/main/java/com/bankInc/BankIncApplication.java
@@ -0,0 +1,13 @@
+package com.bankInc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankIncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankInc/BankIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankInc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankIncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
